9bc39d5e7a5ecaafbb5d5a653fce84117155dfa3
mcarthurgill <mickgill2008@gmail.com>
2013-06-25 15:03:17 -0700

    updated

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,12 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
-
+ 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  @input = params[:user_input]
+  case @input
+	  when "BYE GRANDMA" then redirect to('/?grandma=TIL NEXT TIME, SWEETIE!')
+	  when @input.upcase then redirect to('/?grandma=NO, NOT SINCE 1945!')
+	  else redirect to('/?grandma=WHAT?! SPEAK UP, KIDDO!')
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
+	$('#submit_button').on("click", function notify(e) {
+		e.preventDefault(); 
+		var serialized_word = $("input").serialize();
+		// console.log($("input").serialize());
+		$.ajax({
+			type: "POST",
+			url: '/grandma',
+			data: serialized_word,
+			dataType: 'string',
+		});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
